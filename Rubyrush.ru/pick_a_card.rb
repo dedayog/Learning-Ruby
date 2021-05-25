c04637d1f0e09a3461d46ccf3cdc66e772717b51
dedayog <dedayog@gmail.com>
2021-05-25 23:32:09 +0700

    add 'encrypt' by hash in pick_a_card

--- Rubyrush.ru/pick_a_card.rb
+++ Rubyrush.ru/pick_a_card.rb
@@ -1,5 +1,20 @@
 # Pick a card
 vals = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-shirts = %w(diamonds spades hearts clubs)
+# shirts = %w(diamonds spades hearts clubs)
 shirts_image = %w(♦ ♣ ♥ ♠)
-puts "#{vals.sample}#{shirts_image.sample}"
+print '[ '
+print "#{vals.sample}#{shirts_image.sample} "
+print '] '
+
+require 'digest/md5'
+require 'digest/sha1'
+require 'digest/sha2'
+
+print 'Input string for \'encrypt\': '
+string_for_encode = STDIN.gets.chomp
+print 'MD5: '
+p Digest::MD5.hexdigest(string_for_encode)
+print 'SHA1: '
+p Digest::SHA1.hexdigest(string_for_encode)
+print 'SHA2: '
+p Digest::SHA2.hexdigest(string_for_encode)
